85bbcd21049770808d1180a8e471681a2665ccd3
ivy bixler <ivybixler@gmail.com>
2022-06-25 08:39:19 -0400

    mobile is less broken now content soon

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+function burgerMenu() {
+    console.log("boobiew")
+
+    var x = document.getElementById("respoNav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+
+}
+
+function replace() {
+    console.log("tst")
+    eva.replace()
+    console.log("hello")
+}
