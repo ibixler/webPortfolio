0bb747db5d09f7c13e6ee5223c1d1d8baed07cfb
ibixler <108148378+ibixler@users.noreply.github.com>
2022-07-11 16:08:51 -0400

    Added js to resize mobile dropdown

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,18 @@ function replace() {
     eva.replace()
     console.log("hello")
 }
+
+var root = document.querySelector(':root');
+
+
+function mobileResize() {
+    var rootStyles = getComputedStyle(root);
+    if (window.innerWidth > 600) {
+        console.log("wide enough to not worry just yet");
+    }
+    else{
+        let width = window.innerWidth;
+        root.style.setProperty('--mobileWidth', window.innerWidth - 8 + 'px')
+    }
+}
+
