7f86fcfd98d5fd42b16e8e3ce19198a17dc38506
ibixler <108148378+ibixler@users.noreply.github.com>
2022-10-24 10:29:37 -0400

    just some changes to make the spotify example work

--- web-api-auth-examples-master/authorization_code/app.js
+++ web-api-auth-examples-master/authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '20e0c6c7a4cd4878817497f5156546b4'; // Your client id
+var client_secret = '3e047a9862ec42758a143be3951b80b1'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
